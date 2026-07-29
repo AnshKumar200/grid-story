62557a14192c05a45462481c8891f9e4753433d7
Ansh Kumar <ansh0kumar07@gmail.com>
2026-07-29 11:26:43 +0530

    updated backend url

--- constants/config.ts
+++ constants/config.ts
@@ -1,8 +1,8 @@
 export const CANVAS_WIDTH = 200;
 export const CANVAS_HEIGHT = 200;
 export const PIXEL_SIZE = 2;
-const isLocal = true;
-export const API_URL = (isLocal ? "http://localhost:7878" : 'https://grid-story.onrender.com')
+const isLocal = false;
+export const API_URL = (isLocal ? "http://localhost:7878" : 'https://grid-story-csu6.onrender.com')
 export const WS_URL = (isLocal ? "ws://localhost:7878" : 'wss://grid-story.onrender.com')
 
 export const COLOR_PALETTE = [
